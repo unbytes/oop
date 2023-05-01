a64258c9e9e1022db772e667adc2a8f23a2ea68c
mateusvrs <mateusvrs.dev@gmail.com>
2023-05-01 17:09:37 -0300

    feat(models/store): correct remove branch access using password

--- src/models/Store.java
+++ src/models/Store.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public abstract class Store {
     private static final String name = "Drogas Licitas";
+    private static final String password = "d93724a2";
     private static ArrayList<Branch> branches = new ArrayList<Branch>();
     private static ArrayList<Client> clients = new ArrayList<Client>();
 
@@ -11,17 +12,16 @@ public abstract class Store {
         branches.add(branch);
     }
 
-    public static void removeBranch(Branch branch) throws Exception{
-        if (this.isAuthenticated) {
-            if (this.branches.contains(branch)) {
-                this.branches.remove(branch);
+    public static void removeBranch(Branch branch, String pass) throws Exception {
+        if (password.equals(pass)) {
+            if (branches.contains(branch)) {
+                branches.remove(branch);
             } else {
                 throw new IllegalArgumentException("Branch not found");
             }
         } else {
             throw new IllegalAccessException("You are not authenticated to remove a branch");
         }
-
     }
 
     public static void listAllBranches() {
