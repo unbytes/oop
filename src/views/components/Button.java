af7180adad87117af0a13d688b9070752ec4d6df
Mateus Vieira Rocha da Silva <mateusvrs.dev@gmail.com>
2023-06-08 13:09:09 -0300

    feat(components/button): add rounded border option

--- src/views/components/Button.java
+++ src/views/components/Button.java
@@ -1,13 +1,55 @@
 package views.components;
 
-import java.awt.Color;
-import javax.swing.JButton;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Button extends JButton {
+
     public Button(String text) {
         super(text);
+        setUp();
+        enableRoundedBorder(8);
+    }
+
+    public Button(Icon icon) {
+        super(icon);
+        setUp();
+    }
+
+    public void setUp() {
         this.setBackground(Color.WHITE);
+        this.setForeground(new Color(12, 12, 12));
         this.setFocusable(false);
-        this.setBorderPainted(false);
+        this.setBorder(null);
+        this.setPreferredSize(new Dimension(200, 30));
+    }
+
+    public void enableRoundedBorder(Integer radius) {
+        this.setBorder(new RoundedBorder(radius));
+    }
+
+    public void disableRoundedBorder() {
+        this.setBorder(null);
+    }
+
+    private static class RoundedBorder implements Border {
+        private int radius;
+
+        public RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+        }
+
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        }
     }
 }
