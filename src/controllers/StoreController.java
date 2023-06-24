ab3be0caf8eda21efc98054feb120a167535a01a
MateusVrs <mateusvrs.dev@gmail.com>
2023-06-24 11:37:14 -0300

    feat(controller): add cliet crud methods from model

--- src/controllers/StoreController.java
+++ src/controllers/StoreController.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import java.util.*;
-
+import models.Client;
 import models.Branch;
 import models.Store;
 
@@ -9,7 +9,7 @@ public class StoreController {
 
     public String[] getBranchesAsHTMLTemplate() {
         ArrayList<Branch> branches = Store.getBranches();
-        Integer numberOfBranches = Store.getBranches().size();
+        Integer numberOfBranches = branches.size();
 
         String branchesAsHTMLTemplate[] = new String[numberOfBranches];
         for (Integer index = 0; index < numberOfBranches; index++) {
@@ -28,15 +28,53 @@ public class StoreController {
 
         return branchesAsHTMLTemplate;
     }
-    
-    public boolean authenticateStore(String password){
-        if (password.equals(Store.getpassword())){
+
+    public String[] getClientesAsHTMLTemplate() {
+        ArrayList<Client> clients = Store.getClients();
+        Integer numberOfClients = clients.size();
+
+        String clientsAsHTMLTemplate[] = new String[numberOfClients];
+        for (Integer index = 0; index < numberOfClients; index++) {
+            Client client = clients.get(index);
+            String HTMLTemplate = String.format("""
+                    <html>
+                        <body>
+                            CPF: %s
+                            <br>
+                            Nome: %s
+                        </body>
+                    </html>
+                    """, client.getCPF(), client.getName());
+            clientsAsHTMLTemplate[index] = HTMLTemplate;
+        }
+
+        return clientsAsHTMLTemplate;
+    }
+
+    public boolean authenticateStore(String password) {
+        if (password.equals(Store.getpassword())) {
             return true;
-        } else return false;
+        } else
+            return false;
     }
 
     public void removeBranch(String branchUUID) {
         Branch branch = Store.getBranches().stream().filter(b -> b.getId().equals(branchUUID)).findFirst().orElse(null);
         Store.removeBranch(branch);
+    }
+
+    public void removeClient(String clientUUID) {
+        Client client = Store.getClients().stream().filter(c -> c.getCPF().equals(clientUUID)).findFirst().orElse(null);
+        Store.removeClient(client);
+    }
+
+    public void createClient(String name, Integer age, String CPF) {
+        Client client = new Client(name, CPF, age);
+        Store.registerClient(client);
+    }
+
+    public String getClientName(String clientCPF) {
+        Client client = Store.getClients().stream().filter(c -> c.getCPF().equals(clientCPF)).findFirst().orElse(null);
+        return client.getName();
     }
 }
