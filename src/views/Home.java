cf96b6e278536099b71f55ac7194dab15c394523
MateusVrs <mateusvrs.dev@gmail.com>
2023-06-24 11:34:34 -0300

    feat(views/home): remove clients button from home

--- src/views/Home.java
+++ src/views/Home.java
@@ -2,10 +2,12 @@ package views;
 
 import java.awt.*;
 import views.components.Button;
+import views.components.Title;
 import views.layouts.BasicFrame;
 
 public class Home extends BasicFrame {
     public Home() {
+
         super();
 
         makeBody();
@@ -16,6 +18,10 @@ public class Home extends BasicFrame {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
 
+        Title title = new Title("Bem vindo");
+        gbc.gridy = 0;
+        bodyPanel.add(title, gbc);
+
         Button loginBranchesButton = new Button("Filiais");
         loginBranchesButton.addActionListener(e -> {
             this.dispose();
@@ -23,14 +29,6 @@ public class Home extends BasicFrame {
         });
         gbc.gridy = 1;
         bodyPanel.add(loginBranchesButton, gbc);
-
-        Button clientsButton = new Button("Clientes");
-        clientsButton.addActionListener(e -> {
-            this.dispose();
-            new Client();
-        });
-        gbc.gridy = 2;
-        bodyPanel.add(clientsButton, gbc);
 
         this.add(bodyPanel, BorderLayout.CENTER);
     }
