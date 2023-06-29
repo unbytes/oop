c4865a783df1716985fe45213dccd79db633def2
Mateus Vieira Rocha da Silva <mateusvrs.dev@gmail.com>
2023-06-29 08:45:47 -0300

    test(branch-controller): check html template of the medicament and cosmetic

--- src/tests/TestBranchController.java
+++ src/tests/TestBranchController.java
@@ -6,6 +6,7 @@ import java.util.*;
 import controllers.BranchController;
 import models.Address;
 import models.Branch;
+import models.Cosmetic;
 import models.Medicament;
 import models.Store;
 
@@ -38,14 +39,58 @@ public class TestBranchController {
 		Branch branch1 = new Branch(password, address1);
 		Store.registerBranch(branch1);
 
-		Medicament dipirona = new Medicament("Dipirona", 21, "", 500, 15);
+		Medicament dipirona = new Medicament("Dipirona", 21, "Vermelha", 500, 15);
+		Cosmetic shampoo = new Cosmetic("Shampoo", 210, "Kérastase", "Cabelo", false);
 
 		branch1.login(password);
 		branch1.addProduct(dipirona, 1);
+		branch1.addProduct(shampoo, 5);
 		branch1.logout();
 
 		String[] products = branchController.searchProductsByWordAsHTMLTemplate(branch1.getId(), "Dipirona");
 		assertEquals(1, products.length);
+
+		products = branchController.searchProductsByWordAsHTMLTemplate(branch1.getId(), "");
+		assertEquals("""
+				<html>
+				    <body>
+				        Tipo de Produto: Medicamento
+				        <br>
+				        Produto: Dipirona
+				        <br>
+				        Preço: 21
+				        <br>
+				        Quantidade: 1
+				        <br>
+				        Cor da Caixa: Vermelha
+				        <br>
+				        Dosagem (mg): 500
+				        <br>
+				        Idade Mínima: 15
+				        <br> 
+				    </body>
+				</html>
+				""", products[0]);
+		assertEquals("""
+				<html>
+				    <body>
+				        Tipo de Produto: Cosmético
+				        <br>
+				        Produto: Shampoo
+				        <br>
+				        Preço: 210
+				        <br>
+				        Quantidade: 5
+				        <br>
+				        Marca: Kérastase
+				        <br>
+				        Tipo: Cabelo
+				        <br>
+				        Proteção Solar: Não
+				        <br> 
+				    </body>
+				</html>
+				""", products[1]);
 
 		products = branchController.searchProductsByWordAsHTMLTemplate(branch1.getId(), "Wrong");
 		assertNull(products);
