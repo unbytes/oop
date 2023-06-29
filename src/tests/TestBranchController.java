c2e46fb2cef2a68c2b05421f6c706a05bbad9c8d
Mateus Vieira Rocha da Silva <mateusvrs.dev@gmail.com>
2023-06-28 23:50:44 -0300

    test(branch-controller): check if search product as html template return nº products

--- src/tests/TestBranchController.java
+++ src/tests/TestBranchController.java
@@ -4,16 +4,25 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
 import controllers.BranchController;
+import models.Address;
 import models.Branch;
+import models.Medicament;
 import models.Store;
 
 public class TestBranchController {
+	private final String password = "12345";
 	private BranchController branchController = new BranchController();
+
+	@Before
+	public void setUp() {
+		ArrayList<Branch> branches = Store.getBranches();
+		ArrayList<Branch> clients = Store.getBranches();
+		branches.clear();
+		clients.clear();
+	}
 
 	@Test
 	public void testCreateBranch() {
-		String password = "123456";
-
 		branchController.registerBranch(password, "Brasília", "Gama");
 		branchController.registerBranch(password, "São Paulo", "Setor Bueno");
 
@@ -21,5 +30,24 @@ public class TestBranchController {
 		assertEquals(2, branches.size());
 		assertEquals("Brasília | Gama\n", branches.get(0).getAddress().toString());
 		assertEquals("São Paulo | Setor Bueno\n", branches.get(1).getAddress().toString());
+	}
+
+	@Test
+	public void testSearchProductByWordAsHTMLTemplate() {
+		Address address1 = new Address("São Paulo", "Pinheiros");
+		Branch branch1 = new Branch(password, address1);
+		Store.registerBranch(branch1);
+
+		Medicament dipirona = new Medicament("Dipirona", 21, "", 500, 15);
+
+		branch1.login(password);
+		branch1.addProduct(dipirona, 1);
+		branch1.logout();
+
+		String[] products = branchController.searchProductsByWordAsHTMLTemplate(branch1.getId(), "Dipirona");
+		assertEquals(1, products.length);
+
+		products = branchController.searchProductsByWordAsHTMLTemplate(branch1.getId(), "Wrong");
+		assertNull(products);
 	}
 }
