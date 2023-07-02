e935fb9e83786df82d1feabdd0f26bac2cc8566e
MateusVrs <mateusvrs.dev@gmail.com>
2023-07-02 16:19:28 -0300

    docs(components): create docs for the view components
    Co-authored-by: GabrielCastelo-31 <gabflubr@outlook.com>

--- src/views/components/Title.java
+++ src/views/components/Title.java
@@ -3,7 +3,19 @@ package views.components;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Classe que representa um título de uma página.
+ * 
+ * @author Mateus, Henrique e Gabriel
+ * @version 1.0
+ * @since 2023
+ */
 public class Title extends JLabel {
+    /**
+     * Cria um título com o texto passado.
+     * 
+     * @param text <code>String</code> Texto do título.
+     */
     public Title(String text) {
         super(text);
         this.setFont(new Font("Arial", Font.BOLD, 20));
